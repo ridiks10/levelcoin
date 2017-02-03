9453c59fb6d20cdb6ee81fc22139f5cddeaa20f0
ridiks10 <ridiks100@gmail.com>
2017-02-03 05:45:00 +0200

    hashMerkleRoot 0x7070cb8a1cde19cf4b124faeb1e21e271e4343252ddece8c5f68fa909dca0636

--- src/main.cpp
+++ src/main.cpp
@@ -2883,7 +2883,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0x7070cb8a1cde19cf4b124faeb1e21e271e4343252ddece8c5f68fa909dca0636"));
         if (true && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
